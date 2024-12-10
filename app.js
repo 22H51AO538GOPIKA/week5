d4d595f06a32390d502c013dc001e8d90c5446a7
22H51AO538GOPIKA <mgopika1703@gmail.com>
2024-12-10 18:33:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send('hello');
 });
-
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+const port=3000;
+app.listen(3000,()=>{
+    console.log(`server running on port 3000`);
 });
